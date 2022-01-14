ab63e044c083acf7c4dc978e92ae8486fde594e3
Sameer <sameer@Sameers-MacBook-Pro.local>
2022-01-14 17:29:33 -0600

    basic layout and structure set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import './App.css';
-import TaskCreation from './components/TaskCreation';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-function App() {
+import {Form,Button} from "react-bootstrap";
+class App extends React.Component {
+  state = {
+    inputVal: "",
+
+    //pull toDoTasks from backend
+    toDoTasks: [],
+
+    //pull completedTasks from backend
+    completedTasks: []
+
+  };
+
+  onCreateTask = () =>{
+    alert(this.state.inputVal);
+    var arr = this.state.toDoTasks.concat(this.state.inputVal);
+    this.setState({toDoTasks: arr});
+
+  };
+
+  renderToDoItems(){
+    return this.state.toDoTasks.map(task =>{
+      return <h2>{task}</h2>
+    });
+  }
+
+  render(){
   return (
     <div>
-    <TaskCreation></TaskCreation>
+       <Form>
+     <div>
+        <Form.Label  htmlFor="task">Enter Task</Form.Label>
+        <Form.Control
+            placeholder ="task"
+            onChange={e => this.setState({inputVal: e.target.value})}
+            type = "text"
+            id="taskInput"/>
+            <br></br>
+             <Button variant="primary" onClick={this.onCreateTask}>Create</Button>
+            </div>
+          </Form>
+
+    <br></br>
+
+    <h2>To Do:</h2>
+
+    <div> {this.renderToDoItems()}</div>
+    
+
+     
+    <h2>Complete</h2>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Form,Button} from "react-bootstrap";
+class ToDoItem extends React.Component{
+
+    render() {
+        return (
+            <Form>
+     <div>
+              <h3></h3>
+             <Button variant="secondary">Done</Button>
+        </div>
+          </Form>
+        )
+    }
+
+}
+
+export default ToDoItem;
